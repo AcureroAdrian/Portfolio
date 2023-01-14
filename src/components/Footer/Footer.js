24e43a4cf0b19d4b55b2e15e745991d808d1efa7
Adrian Acurero <adrianacurero@gmail.com>
2023-01-14 11:14:46 -0400

    Fixed Links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,53 @@
-import {useEffect, useRef} from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
-import { SectionTitle, SectionDivider } from '../../styles/GlobalComponents';
+import { useEffect, useRef } from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
+import { SectionTitle, SectionDivider } from "../../styles/GlobalComponents";
 
 const Footer = () => {
-
-  const mail = useRef()
-  const number = useRef()
+  const mail = useRef();
+  const number = useRef();
 
   useEffect(() => {
-      const encEmail = "YWRyaWFuYWN1cmVyb0BnbWFpbC5jb20=";
-      const encTel = "KzU4IDQyNC02MDE2ODM3"
+    const encEmail = "YWRyaWFuYWN1cmVyb0BnbWFpbC5jb20=";
+    const encTel = "KzU4IDQyNC02MDE2ODM3";
 
-      mail.current.setAttribute("href", "mailto:".concat(Buffer.from(encEmail, 'base64' )));
-      mail.current.innerHTML = Buffer.from(encEmail, 'base64')
+    mail.current.setAttribute(
+      "href",
+      "mailto:".concat(Buffer.from(encEmail, "base64"))
+    );
+    mail.current.innerHTML = Buffer.from(encEmail, "base64");
 
-      number.current.setAttribute("href", "tel:".concat(Buffer.from(encTel, 'base64' )));
-      number.current.innerHTML = Buffer.from(encTel, 'base64')
-  }, [mail, number])
-  
+    number.current.setAttribute(
+      "href",
+      "tel:".concat(Buffer.from(encTel, "base64"))
+    );
+    number.current.innerHTML = Buffer.from(encTel, "base64");
+  }, [mail, number]);
 
   return (
     <FooterWrapper>
-    <SectionDivider />
-    <br />
-      <SectionTitle>
-        Contáctame
-      </SectionTitle>
+      <SectionDivider />
+      <br />
+      <SectionTitle>Contáctame</SectionTitle>
       <LinkList>
         <LinkColumn>
           <LinkTitle>WhatsApp o Telegram</LinkTitle>
-          <LinkItem ref={number} >Error al cargar la desencriptación</LinkItem>
+          <LinkItem ref={number}>Error al cargar la desencriptación</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Correo</LinkTitle>
-          <LinkItem ref={mail} >Error al cargar la desencriptación</LinkItem>
+          <LinkItem ref={mail}>Error al cargar la desencriptación</LinkItem>
         </LinkColumn>
       </LinkList>
       <SocialIconsContainer>
@@ -43,14 +55,20 @@ const Footer = () => {
           <Slogan>Nunca parar de aprender</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href='https://www.instagram.com/acureroadrian/'>
-            <AiFillGithub size='3rem' />
+          <SocialIcons target="_black" href="https://github.com/Adr-AA">
+            <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://www.linkedin.com/in/adrian-acurero/'>
-            <AiFillLinkedin size='3rem' />
+          <SocialIcons
+            target="_black"
+            href="https://www.linkedin.com/in/adrian-acurero/"
+          >
+            <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href='https://www.instagram.com/acureroadrian/'>
-            <AiFillInstagram size='3rem' />
+          <SocialIcons
+            target="_black"
+            href="https://www.instagram.com/acureroadrian/"
+          >
+            <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
